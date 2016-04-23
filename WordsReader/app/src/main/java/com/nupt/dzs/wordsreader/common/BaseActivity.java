4cd3cb407bb8ec7d3e6652ee78a7a8c9e5f057fd
dading <1055850340@qq.com>
2016-04-23 15:41:48 +0800

    baseactivity

--- WordsReader/app/src/main/java/com/nupt/dzs/wordsreader/common/BaseActivity.java
+++ WordsReader/app/src/main/java/com/nupt/dzs/wordsreader/common/BaseActivity.java
@@ -1,32 +1,48 @@
 package com.nupt.dzs.wordsreader.common;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-/**
- * Created by Administrator on 2016/4/23.
- *
- * @version 1.0
- * @copyright by ${COMPANY}
- */
+
 public class BaseActivity extends AppCompatActivity implements IBaseActivity{
 
-
+    Activity mActivity;
     ProgressDialog progressDialog;
+    Toast toast;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+    }
+
     @Override
     public void showToast(String msg) {
-
+        if(null == toast){
+            toast = Toast.makeText(getBaseContext(),msg,Toast.LENGTH_SHORT);
+        }else {
+            toast.setText(msg);
+        }
+        toast.show();
     }
 
     @Override
     public void showLoading(String msg) {
-        if(progressDialog!=null){
-
+        if(null == progressDialog){
+            progressDialog = new ProgressDialog(this);
         }
+        progressDialog.setMessage(msg);
+        progressDialog.show();
     }
 
     @Override
     public void hideLoading() {
-
+        if(null != progressDialog){
+            progressDialog.dismiss();
+        }
     }
 }
